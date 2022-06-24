fc71e823e9e8f12073809235a39d3b3ed85a09d4
Nadia Hawa <nadia@Scottys-MBP.lan>
2022-06-24 00:52:20 -0400

    final

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__name__))
 class Config:
     FLASK_APP = os.environ.get('FLASK_APP')
     FLASK_ENV = os.environ.get('FLASK_ENV')
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = 'MST3K'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://eyzqobwt:Nbg-dLcNC-PrIbskjfVVODnNslfd-QSP@fanny.db.elephantsql.com/eyzqobwt'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # STRIPE_SECRET = 'sk_test_4eC39HqLyjWDarjtT1zdp7dc'
